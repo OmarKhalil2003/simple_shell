7a05be3a26f3fe8794d03f14b432d5d0c6fd6c15
Omar Khalil <khalilomar776@gmail.com>
2023-04-25 09:09:43 -0700

    mycommit

--- main.h
+++ main.h
@@ -8,7 +8,40 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/**
+ * struct builtin_d - Defines the builtins functions.
+ * @built: The name of the build in command.
+ * @f: A pointer to the right builtin function.
+ */
+typedef struct builtin_d
+{
+	char *built;
+	void (*f)(char *);
+} builtin_t;
+
 void execmd(char **argv);
 char *get_location(char *command);
+
+/*Builtin functions*/
+int built_in(char **, char *);
+void (*check_built_ins(char *))(char *);
+void exit_b(char *);
+void env_b(char *);
+void cd_b(char *);
+
+/*functions*/
+extern char **environ;
+
+char **token_interface(char *, const char *, int);
+int count_token(char *, const char *);
+int _strcmp(char *, char *);
+
+/*path handling*/
+char **tokenize(int, char *, const char *);
+char *path_finder(char *);
+int find_path(char *);
+char **tokenize_path(int, char *);
+char *search_directories(char **, char *);
+char *build_path(char *, char *);
 
 #endif
